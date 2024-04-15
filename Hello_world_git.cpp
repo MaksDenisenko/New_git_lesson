7b53f3a52081c3703ab285eddc11ea0a89ab9615
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-04-15 21:30:19 +0300

    Added struct Student, basic functions (input, output, average)

--- Hello_world_git.cpp
+++ Hello_world_git.cpp
@@ -1,20 +1,74 @@
-﻿// Hello_world_git.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+
+
+struct Student
+{
+    char name[100];
+    int age = 0;
+    int marks[100];
+    int marks_count = 0;
+};
+
+double getAvergeMark(Student s)
+{
+    double av = 0;
+    for (int i = 0; i < s.marks_count; i++)
+    {
+        av += s.marks[i];
+    }
+    av /= s.marks_count;
+    return av;
+}
+
+void fillStudentByHand(Student& s)
+{
+    printf("Введите имя: ");
+    do
+        gets_s(s.name, 100);
+    while (s.name[0] == '\n' || s.name[0] == '\0');
+    printf("Введите возраст: ");
+    scanf_s("%i", &s.age);
+    printf("Введите количество оценок: ");
+    scanf_s("%i", &s.marks_count);
+
+    for (int i = 0; i < s.marks_count; i++)
+    {
+        printf("Введите оценку №%i: ", i + 1);
+        scanf_s("%i", &s.marks[i]);
+    }
+}
+
+void printStudent(Student s, bool detailed = false)
+{
+    printf("Студент: %s, возраст: %i, оценок: %i\n", s.name, s.age, s.marks_count);
+    if (detailed)
+    {
+        printf("Оценки: ");
+        for (int i = 0; i < s.marks_count; i++)
+        {
+            printf("%i", s.marks[i]);
+        }
+        printf("\n");
+    }
+}
 
 int main()
 {
+    system("chcp 1251");
+    int s_counter = 2;
+    Student group[50];
+
+    for (int i = 0; i < s_counter; i++)
+    {
+        fillStudentByHand(group[i]);
+    }
+
+    for (int i = 0; i < s_counter; i++)
+    {
+        printStudent(group[i]);
+    }
+
+    system("pause");
     std::cout << "Hello World!\n";
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
